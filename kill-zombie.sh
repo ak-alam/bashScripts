742126cee0de196852878523665f9f937a395a77
akbar.alam <alam.akbar1989@gmail.com>
2022-06-13 17:35:55 +0500

    zombie process kill script update

--- kill-zombie.sh
+++ kill-zombie.sh
@@ -3,4 +3,13 @@
 
 #!/bin/bash
 
-echo "Hello"
+kill_zombie_process(){
+   echo "All Zombie Processes."
+   echo $(ps aux | egrep "Z|defunct")
+   echo ##############################
+   parent_proc_id=$(ps axo ppid,stat | grep Z | awk '{print $1}')
+   echo "Parent process ID: $parent_proc_id"
+   echo "Killing Zombie Process $parent_proc_id"
+   kill -9 $parent_proc_id
+}
+kill_zombie_process
